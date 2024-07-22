59fb6820e1d66cc9f17ba6578a4f20ba33e20f98
efa07 <efatariku07@gmail.com>
2024-07-22 21:09:13 +0300

    pycodestyle fixed

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,11 +1,11 @@
 def index_range(page, page_size):
     """
     Calculate the start and end index for a given page and page_size.
-    
+
     Args:
     - page (int): The current page number (1-indexed).
     - page_size (int): The number of items per page.
-    
+
     Returns:
     - tuple: A tuple containing the start index and end index.
     """

cc7c91492557c69e9eb7513ce9bf286ef0e49eac
efa07 <efatariku07@gmail.com>
2024-07-30 21:32:33 +0300

    make file executable

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)

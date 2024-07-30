94e6be4fe27cd6d5fa289c691beba03ce163f817
efa07 <efatariku07@gmail.com>
2024-07-30 22:40:25 +0300

    pycodestyle fixed

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -7,6 +7,7 @@ from flask_babel import Babel, _
 
 app = Flask(__name__)
 
+
 class Config:
     """
     Configuration class for Flask-Babel.
@@ -15,24 +16,31 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
 
-# Instantiate the Babel object
+
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
     """
-    Determines the best language match based on the request's Accept-Language headers.
+    Determines the best language match based on
+    the request's Accept-Language headers.
     """
+
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index():
     """
     Renders the index.html template with translated content.
     """
+
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)

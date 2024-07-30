d78aa238840a8b0f545620d707b66812c5f4af7e
efa07 <efatariku07@gmail.com>
2024-07-30 23:13:58 +0300

    chmod 777

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -7,6 +7,7 @@ from flask_babel import Babel, _
 
 app = Flask(__name__)
 
+
 class Config:
     """
     Configuration class for Flask-Babel.
@@ -14,6 +15,7 @@ class Config:
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app.config.from_object(Config)
 
@@ -27,6 +29,7 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
+
 def get_user():
     """
     Retrieves the user based on the 'login_as' parameter in the query string.
@@ -35,25 +38,32 @@ def get_user():
     user_id = request.args.get('login_as')
     if user_id:
         return users.get(int(user_id))
+
     return None
+
 
 @app.before_request
 def before_request():
     """
     Runs before each request. Sets g.user to the user retrieved by get_user.
     """
+
     g.user = get_user()
+
 
 @babel.localeselector
 def get_locale():
     """
-    Determines the best language match based on the request's Accept-Language headers
+    Determines the best language match based on the
+    request's Accept-Language headers
     or locale parameter in the query string.
     """
+
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index():
@@ -61,6 +71,7 @@ def index():
     Renders the index.html template with translated content.
     """
     return render_template('5-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
